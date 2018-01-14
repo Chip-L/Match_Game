b7bb829398aeb0ff111263013638fccb18f5fb20
Chip-L <chip_long@yahoo.com>
2018-01-14 02:47:34 -0700

    Added settings div - no functionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,11 +4,32 @@
 $(document).ready( function () {
   createMenu();
   MatchGame.playGame();
-});
+} );
 
 function createMenu() {
-  $(".rule").click(function(){
-    $("#rules").toggle();
+  // show/hide rules
+  $('.rule').click( function () {
+    $('#rules').toggle();
     // TODO: pause timer
-  });
+  } );
+
+  // show/hide display from menu button
+  $('.settings').click( function () {
+    showSettings();
+  } );
+};
+
+function showSettings () {
+  $('#settings').show();
+  $('.settings').click( function () {
+    cancelSettings();
+  } );
+};
+
+function cancelSettings () {
+  $('#settings').hide();
+  $('.settings').click( function () {
+    showSettings();
+  } );
+  // TODO: clean up the cancellation (if needed)
 }
