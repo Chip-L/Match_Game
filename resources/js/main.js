bab06e06c8bbfffe521b05b5902494c903f11efc
Chip-L <chip_long@yahoo.com>
2018-01-14 02:16:35 -0700

    no bootstrap started header started rules display archiving old bootstrap files

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,5 +2,13 @@
   Sets up a new game after HTML document has loaded.
 */
 $(document).ready( function () {
+  createMenu();
   MatchGame.playGame();
 });
+
+function createMenu() {
+  $(".rule").click(function(){
+    $("#rules").toggle();
+    // TODO: pause timer
+  });
+}
